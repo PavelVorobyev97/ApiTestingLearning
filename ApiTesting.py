e7cf96c4d7a156b6dead5da9ebf516f270a999ff
PavelVorobyev97 <pavelvorobyev97@yandex.ru>
2023-04-25 23:41:02 +0600

    wrapped get in method

--- ApiTesting.py
+++ ApiTesting.py
@@ -1,15 +1,21 @@
 import requests
 import json
-import jsonpath 
+import jsonpath
 
-url = 'https://reqres.in/api/users?page=2'
-# Get response
-response = requests.get(url)
+def testGetResquest():
+    url = 'https://reqres.in/api/users?page=2'
+    # Get response
+    response = requests.get(url)
 
-# Parse JSON 
-json_response = json.loads(response.text)
+    # Parse JSON 
+    json_response = json.loads(response.text)
 
-# Fetch json value
-total = jsonpath.jsonpath(json_response, 'total')
-print(total)
-print(response)
+    # Fetch json value
+    total = jsonpath.jsonpath(json_response, 'total')
+    print(total[0])
+    print(response.status_code)
+    assert response.status_code == 200    
+
+testGetResquest()
+# print(total)
+# print(response)
