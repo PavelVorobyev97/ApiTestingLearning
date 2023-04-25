d717eb18741c27a384e69fc1837b1102ca4b19b9
PavelVorobyev97 <pavelvorobyev97@yandex.ru>
2023-04-25 23:55:00 +0600

    fix typo, add testDelete

--- ApiTesting.py
+++ ApiTesting.py
@@ -2,7 +2,7 @@ import requests
 import json
 import jsonpath
 
-def testGetResquest():
+def testGetRequest():
     url = 'https://reqres.in/api/users?page=2'
     # Get response
     response = requests.get(url)
@@ -10,12 +10,23 @@ def testGetResquest():
     # Parse JSON 
     json_response = json.loads(response.text)
 
-    # Fetch json value
+    # Fetch total json value
     total = jsonpath.jsonpath(json_response, 'total')
-    print(total[0])
-    print(response.status_code)
-    assert response.status_code == 200    
+    # print(total[0])
+    print("Get /users?page=2 code:" + str(response.status_code))
 
-testGetResquest()
-# print(total)
-# print(response)
+    # Assert status code
+    assert response.status_code == 200
+
+def testDelete():
+    url = 'https://reqres.in/api/users/2'
+    # Get response
+    response = requests.delete(url)
+    
+    print("Delete /users/2 code:" + str(response.status_code))
+    
+    # Assert status code
+    assert response.status_code == 204    
+
+testGetRequest()
+testDelete()
